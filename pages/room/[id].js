2e480ce8698756482e0d8b75df397e9bd6d501fb
fsarrias <fsarrias@kolab.es>
2022-07-20 14:52:35 +0200

    peerjs

--- pages/room/[id].js
+++ pages/room/[id].js
@@ -1,7 +1,21 @@
+import Script from 'next/script'
+
 export default function Room() {
     return (
+        <>
+      <Script
+        src='https://unpkg.com/peerjs@1.4.5/dist/peerjs.min.js'
+        onLoad={() => {
+            console.log(Peer)
+        }}
+      />
+
+
       <h1 className='mt-20 text-center text-3xl uppercase font-black'>
         Room
       </h1>
+
+      </>
+
     )
   }
